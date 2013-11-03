6eeb6a4e315608324bd932ee2c25127c51e3d8c8
anngelfra <ann.gel.fra@gmail.com>
2013-11-03 16:47:16 -0600

    UPDATED: New functions added to main.js

--- js/main.js
+++ js/main.js
@@ -14,13 +14,15 @@ $(document).ready(function(){
     });
 });
 
-function addPosts(data){
-    var obj = JSON.parse(data);
-    alert(obj.length);
-    $("#contactResult").html(data);
+function fillYouTubePost(title, URL){
     var template = '<div class="post">';
-    template += '<iframe width="560" height="315" src="//www.youtube.com/embed/DfOAgO70jG8" frameborder="0" allowfullscreen></iframe>';
-    template += '<p>Description</p><hr/>';
-    template += '<p>Author</p>';
+    template += '<iframe width="560" height="315" src="'+URL+'" frameborder="0" allowfullscreen></iframe>';
+    template += '<p>'+title+'</p><hr/>';
     template += '</div>';
+    return template;
+}
+
+function fillFacebookPost(title, URL){
+    var template = '<div class="post">';
+
 }
