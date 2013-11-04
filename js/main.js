87ef9a27380e585b29b210d269b620518a96f120
mellamosilvestre <mellamosilvestre@gmail.com>
2013-11-04 00:28:28 -0600

    added/updated: loading spinner to both pages

--- js/main.js
+++ js/main.js
@@ -1,53 +1,57 @@
 $(document).ready(function(){
-    //$( "#contact" ).submit(function( event ) {
-        //event.preventDefault();
-        var opts = {
-            lines: 8, // The number of lines to draw
-            length: 6, // The length of each line
-            width: 7, // The line thickness
-            radius: 8, // The radius of the inner circle
-            corners: 1, // Corner roundness (0..1)
-            rotate: 0, // The rotation offset
-            direction: 1, // 1: clockwise, -1: counterclockwise
-            color: '#000', // #rgb or #rrggbb or array of colors
-            speed: 1, // Rounds per second
-            trail: 60, // Afterglow percentage
-            shadow: false, // Whether to render a shadow
-            hwaccel: false, // Whether to use hardware acceleration
-            className: 'spinner', // The CSS class to assign to the spinner
-            zIndex: 2e9, // The z-index (defaults to 2000000000)
-            top: '0px', // Top position relative to parent in px
-            left: '0px' // Left position relative to parent in px
-        };
-        var target = document.getElementById('loadingDiv');
-        var spinner = new Spinner(opts).spin(target);
+
+    var rightSpinnerTarget = document.getElementById('right-panel-loading');
+    var leftSpinnerTarget = document.getElementById('left-panel-loading');
+
+    //we suppose you are checking our awesome code for more than the spinner... anyway here it is, enjoy :D http://fgnass.github.io/spin.js/
+    var opts = {
+        lines: 8, // The number of lines to draw
+        length: 6, // The length of each line
+        width: 7, // The line thickness
+        radius: 8, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb or array of colors
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'absolute-center', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: '0px', // Top position relative to parent in px
+        left: '0px' // Left position relative to parent in px
+    };
+    var rightSpinner = new Spinner(opts).spin(rightSpinnerTarget);
+    var leftSpinner = new Spinner(opts).spin(leftSpinnerTarget);
 
 
-
-        $("#loadingDiv").show();
-        var sName = "patzun";
-        var jqxhr = $.post("php/getPosts.php",{
-                keyWord:sName
-            },
-            function(data){
-                var obj = JSON.parse(data);
-                var content = "";
-                var color = feedColor();
-                for(var i=0; i<obj.items.length; i++){
-                    var post = obj.items[i];
-                    if (post.kind == 1) {
-                        content += fillYouTubePost(post.title, post.url, color);
-                    }
-                    if (post.kind == 2){
-                        content += fillFacebookPost(post.albumTitle, post.url, color);
-                    }
+    $("#right-panel-loading").show();
+    var sName = "patzun";
+    var jqxhr = $.post("php/getPosts.php",{
+            keyWord:sName
+        },
+        function(data){
+            var obj = JSON.parse(data);
+            var content = "";
+            var color = feedColor();
+            for(var i=0; i<obj.items.length; i++){
+                var post = obj.items[i];
+                if (post.kind == 1) {
+                    content += fillYouTubePost(post.title, post.url, color);
                 }
-                $("#right-feed").html(content);
-            }).always(function() {
-                $("#loadingDiv").hide();
-            });
+                if (post.kind == 2){
+                    content += fillFacebookPost(post.albumTitle, post.url, color);
+                }
+            }
+            $("#right-feed").html(content);
+        }).always(function() {
+            $("#right-panel-loading").hide();
+        }
+    );
 
-    var sName = "los angeles";
+    $("#left-panel-loading").show();
+    sName = "los angeles";
     $.post("php/getPosts.php",{
             keyWord:sName
         },
@@ -65,8 +69,10 @@ $(document).ready(function(){
                 }
             }
             $("#left-feed").html(content);
-        });
-
+        }).always(function() {
+            $("#left-panel-loading").hide();
+        }
+    );
     //});
 });
 
